19f4c1200b56e4c5e2f844fee21fc4e6eb40abad
hootener <eli@gamewisp.com>
2018-01-01 22:18:55 -0600

    Typo

--- deployCommands.sh
+++ deployCommands.sh
@@ -4,7 +4,7 @@
 docker-compose -f prod.docker-compose.yml up jigsaw_build_assets && docker-compose -f prod.docker-compose.yml up jigsaw_build_site
 
 #commit the builds to master
-git add . && git commit -m "Demployment Commit"
+git add . && git commit -m "Deployment Commit"
 
 #push the build output to gh-pages.
 git subtree push --prefix build_production origin gh-pages
