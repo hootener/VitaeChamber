7b557810988d28c6c13c6f7911c5f024b860343d
Eli Hooten <elir.hooten@gmail.com>
2020-02-23 17:16:29 -0600

    Update Vitae and  add Press

---
+++ source/press.blade.php
@@ -0,0 +1,56 @@
+@extends('_layouts.master')
+@section('page-title')
+Eli Hooten -- Press
+@endsection
+@section('page-style')
+<style>
+ul {
+    list-style: none;
+}
+.press-item{
+    margin-bottom: 1em;
+}
+
+</style>
+
+@endsection
+@section('body')
+<div class="row">
+    <div class="col-md-8 offset-md-2">
+        <h3>Press</h3>
+        <p> Articles, interviews, etc.</p>
+        <ul style="margin-top: 2em">
+            <li class="press-item">
+                <a href="https://mailchi.mp/aa46945249b2/the-done-column" target="_blank" rel="noopener">Five Minute Mentor: The Done Column, Eli Hooten</a> (Southern Alpha: Launch Letter, 2019)
+            </li>
+            <li class="press-item">
+                <a href="https://www.nashvillepost.com/business/technology/article/21028416/gamewisp-team-eyes-services-growth" target="_blank" rel="noopener">GameWisp Team Eyes Services Growth</a> (Nashville Post, 2018)
+            </li>
+            <li class="press-item">
+                <a href="https://www.forbes.com/sites/breebrouwer/2016/09/07/gamewisp-video-game-live-streaming-subscriptions-monetization/#13b54e02633c" target="_blank" rel="noopener">GameWisp's CEO Is On A Mission To Help Video Game Live Streamers Make A Living </a>(Forbes, Sept 2016)
+            </li>
+            <li class="press-item">
+                <a href="https://www.venturenashville.com/video-content-monetizer-gamewisp-raised-843k-has-runway-to-series-a-cms-1250" target="_blank" rel="noopener">Video-content monetizer GameWisp raised $843K, has runway to Series A </a>(Venture Nashville, Feb 2016)
+            </li>
+            <li class="press-item">
+                <a href="https://blogg.livespace.se/twitch-podcast/interview-with-eli-hooten-from-gamewisp/" target="_blank" rel="noopener">Interview with Eli Hooten from GameWisp</a> (Livespace, Sept 2015)
+            </li>
+            <li class="press-item">
+                <a href="https://www.tennessean.com/story/money/tech/2015/06/04/nashvilles-gamewisp-targets-gamers-fans/28507149/" target="_blank" rel="noopener">GameWisp targets gamers, fans </a>(The Tennessean, June 2015)
+            </li>
+            <li class="press-item">
+                <a href="https://www.techstars.com/content/blog/the-techstars-experience-gamewisps-story/" target="_blank" rel="noopener">The Techstars Experience: GameWisp’s Story </a>(TechStars Blog, May 2015)
+            </li>
+            <li class="press-item">
+                <a href="https://www.builtinchicago.org/2014/07/14/how-techstars-gamewisp-taking-over-350-million-person-youtube-gaming-market" target="_blank" rel="noopener">How Techstars’ GameWisp is taking over the 350 million-person YouTube gaming market </a>(BuiltIn Chicago, July 2014)
+            </li>
+            <li class="press-item">
+                <a href="https://www.bizjournals.com/nashville/blog/2014/05/nashville-startup-aims-to-give-music-citys-tech.html" target="_blank" rel="noopener">Nashville startup aims to give Music City's tech community a few extra lives </a>(Nashville Business Journal, May 2014)
+            </li>
+            <li class="press-item">
+                <a href="https://issuu.com/murraystateuniv/docs/b_g_fall09final" target="_blank" rel="noopener">A World of Humans and Machines </a>(Blue and Gold, May 2009)
+            </li>
+        </ul>
+    </div>
+</div>
+@endsection
