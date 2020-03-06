73d111a59ccfd808264194c0053aacf1d767634e
hootener <elir.hooten@gmail.com>
2020-03-05 22:30:52 -0600

    Update configs

--- config.production.php
+++ config.production.php
@@ -1,11 +1,13 @@
 <?php
 
+use Illuminate\Support\Str;
+
 return [
     'environment' => 'prod',
     'baseUrl' => 'https://hootener.github.io/VitaeChamber',
     'production' => true,
     'selected' => function ($page, $section) {
-        return str_contains($page->getPath(), $section) ? 'active' : '';
+        return Str::contains($page->getPath(), $section) ? 'active' : '';
     },
     'build' => [
         'destination' => 'docs',
